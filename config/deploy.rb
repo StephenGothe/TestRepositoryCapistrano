cf873e1e7b04abbd1d70eb8b39ce68221953846d
StephenGothe <stephen.gothe@outlook.de>
2018-07-24 14:23:34 +0200

    New Deploy Message

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "TestRepo"
+set :repo_url, "git@github.com/StephenGothe/TestRepositoryCapistrano.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+ set :stage, :production
+ set :deploy_to, "/var/www/TestRepo"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
